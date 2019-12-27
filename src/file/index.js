c204267410539500247feb14f1364aa6ead17d32
forgere <forgere123zw@gmail.com>
2019-12-27 14:13:11 +0800

    format all import

--- src/file/index.js
+++ src/file/index.js
@@ -30,25 +30,44 @@ const file = {
         })
     },
     dealWithJS: function(files){
-        const rule = /import[\s\S]*{([\S\s]*)}[\s\S]*['"]@bone\/next/;
+        const rule = /import[\s\S]*?{([\S\s]*?)}[\s\S]*?from/g;
         (function next(index) {
             if(index < files.length) {
                 const element = files[index];
-                console.log(element)
+                if (element == path.resolve(__filename)) {
+                    next(index + 1)
+                    return
+                }
                 fs.readFile(element, 'utf8', function (err,data) {
                     if (err) {
                         return console.log(err);
                     }
                     if (data.match(rule)) {
-                        const matchContent = data.match(rule)[1]
-                        if (matchContent) {
-                            const replacement = data.match(rule)[1].split(',').filter(val => val.trim() !== "").map(val => '  '+val.trim()+',').join('\n')
-        
-                            var result = data.replace(matchContent, "\n"+replacement+"\n");
-                            fs.writeFile(element, result, 'utf8', function (err) {
-                                if (err) return console.log(err);
-                                next(index+1)
-                            });
+                        const matchContents = data.match(rule)
+                        if (matchContents.length) {
+                            (function inner(number){
+                                if (number < matchContents.length) {
+                                    let innerRule = /import[\s\S]*{([\S\s]*)}[\s\S]*from/
+                                    const matchContent = matchContents[number].match(innerRule);
+                                    if (matchContent) {
+                                        const replacement = matchContent[1].split(',').filter(val => val.trim() !== "").map(val => '  '+val.trim()+',').join('\n')
+                                        if (matchContent[1].trim() === replacement.trim()) {
+                                            return inner(number+1)
+                                            
+                                        }
+                                        var result = fs.readFileSync(element).toString().replace(matchContent[1], "\n"+replacement+"\n");
+
+                                        fs.writeFile(element, result, 'utf8', function (err) {
+                                            if (err) return console.log(err);
+                                            inner(number + 1)
+                                        });
+                                    }
+                                } else {
+                                    next(index+1)
+                                }
+                            }(0))
+                        } else {
+                            next(index + 1)
                         }
                     } else {
                         next(index+1)
